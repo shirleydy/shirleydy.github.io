566a230025f011ae6d5e6622c5e8e0456f29a57d
Jack Deng <jack.deng@thinkchina.com>
2021-07-06 12:03:18 +1000

    update

--- src/pages/project/meet.js
+++ src/pages/project/meet.js
@@ -3,7 +3,7 @@
  * @Author: Jack Deng
  * @Date: 2021-05-16 14:16:03
  * @LastEditors: Jack Deng
- * @LastEditTime: 2021-06-07 22:13:22
+ * @LastEditTime: 2021-07-06 11:55:07
  * @FilePath: /shirley/src/pages/project/meet.js
  */
 /*
@@ -91,69 +91,71 @@ function Index() {
             return (
               <div className="Meet_wrap4 flex-col">
                 {index === 0 && (
-                  <p
-                    className={"paragraph"}
-                  >{`Roles: This is the project that I responsible for from concepts to implementation and through all the iterations. \n
-                  [The original app was in Chinese and I did some redesign and translated some of the pages into English just for display. ]
-`}</p>
+                  <div className="paragraph">
+                    <p>{`Roles: I am responsible for this project from concepts to the implementation and through all the iterations.`}</p>
+                    <p style={{ fontSize: "12px", color: "#adadad" }}>
+                      {
+                        "[The original app was in Chinese and I did some redesign and translated some of the pages into English for display. ]"
+                      }
+                    </p>
+                  </div>
                 )}
                 {index === 1 && (
                   <p
                     className={"paragraph"}
-                  >{`Background: MEET is an information social platform,  there are many networking apps but most of them are following certain algorithm to show similar contents based on user behavior, so we were trying to do some different, and thus Meet born. We hoped to provide useful and valuable content  based on each user’s needs and based on their likes or dislikes. And users can share different opinions and find person with same value, even soulmate.
+                  >{`Background: MEET is an information social platform, there are many networking apps but most of them are following certain algorithms to show similar content based on user behaviour, so we were trying to do some different, and thus Meet born. We hoped to provide useful and valuable content based on each user’s needs and based on their likes or dislikes. And users can share different opinions and find a person with the same value, even a soulmate.
 `}</p>
                 )}
                 {index === 2 && (
                   <p
                     className={"paragraph"}
-                  >{`According to the data, It can be seen that in China the largest user base which is 63% of social media apps is between 20-29 years old, and 27% is between 30-39 years old.  There is little distinction in gender, basically half and half. based on the data, the general style was defined as youthful, energetic, neutral and concise.`}</p>
+                  >{`According to the data, It can be seen that in China the largest user base among social media apps is between 20-29 years old which is 63%, and 27% is between 30-39 years old. There are a few distinctions in gender, basically half and half. based on the data, the general style was defined as youthful, energetic, neutral and concise.
+                  `}</p>
                 )}
                 {index === 4 && (
                   <p
                     className={"paragraph"}
-                  >{`From user persona, I found the most pain points were there’s no reliable social platforms for young people to find someone that share the same value and same hobby with them, and another pain point was there are lots of information platform out there, and they need to switch from one to another to gain comprehensive information.`}</p>
+                  >{`From user persona, I found the pain points were there are no reliable social platforms for young people to find someone that shares the same value and same hobby with them, and another pain point was there are lots of information platforms out there, and they need to switch from one to another to gain comprehensive information.
+                  `}</p>
                 )}
                 {index === 6 && (
                   <p
                     className={"paragraph"}
-                  >{`The needed features, actions and pieces of information were already pretty clear after the phases above, so I just jumped straight into a quick sketch to map out the information architecture and wireframes. I tried to put in all the features I was planning, and most of it already felt logical at this point.After a couple of paper iterations I had the general idea ready, so I switched to digital version, where it’s easier to move things around.`}</p>
+                  >{`The needed features, actions and pieces of information were already pretty clear after the phases above, so I just jumped straight into a quick sketch to map out the information architecture and wireframes. I tried to put in all the features I was planning, and most of it already felt logical at this point. After a couple of paper iterations, I had a general idea ready, so I switched to the digital version, where it’s easier to move things around.`}</p>
                 )}
                 {index === 10 && (
                   <p className={"paragraph"}>
-                    {`I also did the logo for this app and it made up of 3 shapes. The mainly background shape is a communication bubble that is commonly seen in many apps, and it’s obviously shows the main feather- which is communication. And the Letter M is the first letter of the app’s name and the shape of it is made of two persons holding hands with each other. The overall logo is very easy to recognized and also conveys the main functions of the app.`}
+                    {`I also did the logo for this app and it made up of 3 shapes. The main background shape is a communication bubble that is commonly seen in many apps, and it shows the main feature which is communication. And the Letter M is the first letter of the app’s name and the shape of it is made of two persons holding hands with each other. The overall logo is very easy to recognized and also conveys the main functions of the app.`}
                   </p>
                 )}
                 {index === 11 && (
                   <p className={"paragraph"}>
-                    {`The welcome screen was pretty standard. It features a big app icon to reinforce branding and add some visual delight to the user’s first contact with the app. It also repeats the value proposition which explains the user why it’s worth it to complete the questionnaire.`}
+                    {`The welcome screen was pretty standard. It features a big app icon to reinforce branding and add some visual delight to the user’s first contact with the app. It also repeats the value proposition which explains to the user why it’s worth it to complete the questionnaire.`}
                   </p>
                 )}
                 {index === 14 && (
                   <p className={"paragraph"}>
-                    {`The questionnaire itself is equally simple. The first two pages are mainly focus on age and gender and with some visual illustration in the middle to help catching the user’s eyes and last page filled with multiple choices. The user can go back to change his choices, or go forward to skip a question.`}
+                    {`The questionnaire itself is equally simple. The first two pages are mainly focused on age and gender and with some visual illustration in the middle to help to catch the user’s eyes and the last page filled with multiple choices. The user can go back to change his choices, or go forward to skip a question.`}
                   </p>
                 )}
                 {index === 15 && (
                   <p className={"paragraph"}>
-                    {`If the user presses the Skip button, he immediately gets the view of the inside content. As long as he has not completed the wizard, there is an illustration that repeats the value prop and urges the user to answer the questions.`}{" "}
+                    {`If the user presses the Skip button, he immediately gets a view of the inside content. As long as he has not completed the wizard, there is an illustration that repeats the value prop and urges the user to answer the questions.`}
                   </p>
                 )}
                 {index === 16 && (
                   <p className={"paragraph"}>
-                    {`I also created some cute illustration design on the empty pages , which incorporates the brand ip image ‘dolphin’ into the pages and effectively alleviates users negative emotions and further enhances product experience and brand perception.`}{" "}
+                    {`I also created some cute illustration designs on the empty pages, which incorporates the brand IP image ‘dolphin’ into the pages and effectively alleviates users negative emotions and further enhances product experience and brand perception.`}
                   </p>
                 )}
                 {index === 17 && (
                   <p className="paragraph">
-                    {`The app has 4 buttons on the bottom navigation, which are new, circle , discover,  and me. There are three sub-titles in this New page, which are following, trending, and local. 
-These three parts of information flow are totally customisable. 
-In 'following' section, this page shows users their subscribed content, subscribed topics. For the trending part, it shows the contents with most likes, when users like the content, it increase the temperature of this topic and this page shows the most hottest topic to the users. You can also find a little medal shown on the left top of the topic creator with the most likes, which encourages the creators to post more topics. 
-In the local sub-page, it achieves the design goal of 'information serves life', many local store bargains, sales products or even second-hand product selling information are shown in this section.`}
+                    {`The app has 4 buttons on the bottom navigation, which are new, circle, discover, and me. There are three sub-titles on this New page, which are following, trending, and local. These three parts of information flow are customisable. In the 'following' section, this page shows users their subscribed content, subscribed topics. For the trending part, it shows the content with the most likes, when users like the content, it increases the temperature of this topic and this page shows the hottest topic to the users. You can also find a little medal shown on the left top of the topic creator with the most likes, which encourages the creators to post more topics. In the local sub-page, it achieves the design goal of 'information serves life', many local store bargains, sales products or even second-hand product selling information are shown in this section.`}
                   </p>
                 )}
                 {index === 18 && (
                   <p className="paragraph">
-                    {`Next feature part is the circle function, it’s a high-quality instant gathering place for people share great moments, have same value or interests. There are thousands circles in the app created by different users and every members in the circle get a place to share their thoughts and experience. Here it solves the users difficulties of making friends outside, instead you can easily find a friend in our app.`}
+                    {`The next feature part is the circle function, it’s a high-quality instant gathering place for people to share great moments, have the same value or interests. There are thousands of circles in the app created by different users and every member in the circle get a place to share their thoughts and experience. Here it solves the users' difficulties of making friends outside, instead, you can easily find a friend in our app.`}
                   </p>
                 )}
                 <img className={"Meet_img"} src={image} />
